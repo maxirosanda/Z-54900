c8cd5844ccc402082cf2ff0e8a9b4242349fd0b6
maxirosanda <maxi_rosanda@hotmail.com>
2023-04-15 15:54:46 -0300

    clase1

---
+++ clase1/main.js
@@ -0,0 +1,55 @@
+/*
+Este es un comentario en js
+puede tener varias lineas
+*/
+//Este es el comentario de una linea
+
+//Ejemplo variables(manejando un producto)
+/*
+let precio = 120000;
+let nombre = "Televisor 32 pulgadas";
+let stock = 500;
+const LOTE = "12/12/2022";
+precio = 150000;
+nombre = "TV Smart 32 pulgadas";
+let valorLote = precio * stock
+console.log(valorLote)
+*/
+//Operaciones matematicas simples
+/*
+let numeroA = 12;
+let numeroB = 43;
+let resultado = numeroA / numeroB; // puedo usar + - * /
+console.log(resultado);
+*/
+// Concatenar String
+/*
+let textoA = "Hola estudiantes de";
+let textoB = "Coderhouse";
+let mensaje = textoA + " " + textoB;
+console.log(mensaje);
+let numeroC = 12.5
+let unirStringNumero = textoA + " " + numeroC
+console.log(unirStringNumero)
+console.log(numeroC)
+*/
+//Resumen ejemplo sumador
+let nombrePersona = prompt("Ingrese el nombre de la persona");
+let operador = prompt("Ingrese el operador");
+operador = parseFloat(operador);
+let operando = prompt("ingrese el operando");
+operando = parseFloat(operando)
+let resultado = operador + operando;
+alert("Sr " + nombrePersona + " " + "el resultado de la suma es: " + resultado);
+const PI = 3.14;
+
+
+
+
+
+
+
+
+
+
+
